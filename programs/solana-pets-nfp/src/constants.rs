1d7587b27413ec6fb87606886fbc88f53230d4f8
solaradex <solara.dex@proton.me>
2026-09-18 05:23:38 -0400

    feat: add genetics schema version

--- programs/solana-pets-nfp/src/constants.rs
+++ programs/solana-pets-nfp/src/constants.rs
@@ -2,3 +2,4 @@ pub const GENESIS_SEED: &[u8] = b"genesis";
 pub const GENESIS_MAX_SUPPLY: u16 = 15_000;
 pub const MAX_PET_NAME: usize = 32;
 pub const MAX_SPECIES: usize = 16;
+pub const GENETICS_VERSION_V1: u8 = 1;
